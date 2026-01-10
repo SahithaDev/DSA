a71797795326ecf58385f58c0a509cedbff2b134
Sahitha0203 <sahitha.mutakodoor@gmail.com>
2026-01-10 16:21:14 +0530

    brute force for array rotation

--- Arrays/arrayRotation.js
+++ Arrays/arrayRotation.js
@@ -1,9 +1,11 @@
 let arr = [1, 2, 3, 4, 5];
-let k = 2;
 let n = arr.length;
-
-for (let i = 0; i < n; i++) {
-  for (let j = n - k - 1; j < n; j++) {
-    console.log(arr[j]);
+for (let count = 1; count <= 3; count++) {
+  let temp = arr[n - 1];
+  for (let i = n - 2; i >= 0; i--) {
+    arr[i + 1] = arr[i];
   }
+  arr[0] = temp;
 }
+
+console.log(arr);
